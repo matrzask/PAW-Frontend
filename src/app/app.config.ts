2ff610d85d85d55b5a5fa4dc56afe6c7702ce208
matrzask <maciejtrzask@student.agh.edu.pl>
2025-01-05 17:13:31 +0100

    angularfire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), provideFirebaseApp(() => initializeApp({ projectId: "paw-projekt-aaea9", appId: "1:835096115182:web:e35ee55b798f8d834d40ff", storageBucket: "paw-projekt-aaea9.firebasestorage.app", apiKey: "AIzaSyBirWaTpZ--FH5cFnri3KYV9DdnEaFyomA", authDomain: "paw-projekt-aaea9.firebaseapp.com", messagingSenderId: "835096115182" })), provideFirestore(() => getFirestore()),
   ],
 };
