9697e9cd82e2b7c4d3e3204da2ed517cbb2806de
matrzask <maciejtrzask@student.agh.edu.pl>
2025-01-13 17:17:50 +0100

    top bar

---
+++ src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { SwitchSourceComponent } from '../switch-source/switch-source.component';
+import { LogOutComponent } from '../log-out/log-out.component';
+import { LoginButtonsComponent } from '../login-buttons/login-buttons.component';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../model/user.interface';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-top-bar',
+  imports: [
+    CommonModule,
+    SwitchSourceComponent,
+    LogOutComponent,
+    LoginButtonsComponent,
+    RouterModule,
+  ],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.css',
+})
+export class TopBarComponent {
+  constructor(private authService: AuthService) {}
+
+  user: User | null = null;
+
+  ngOnInit() {
+    this.user = this.authService.currentUserValue.user;
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,14 +2,11 @@ import { Component } from '@angular/core';
 import { CalendarComponent } from '../../components/calendar/calendar.component';
 import { AddAvailabilityComponent } from '../../components/add-availability/add-availability.component';
 import { AddAbsenceComponent } from '../../components/add-absence/add-absence.component';
-import { SwitchDoctorComponent } from '../../components/switch-doctor/switch-doctor.component';
-import { SwitchSourceComponent } from '../../components/switch-source/switch-source.component';
-import { LogOutComponent } from '../../components/log-out/log-out.component';
 import { CommonModule } from '@angular/common';
 import { User } from '../../model/user.interface';
 import { AuthService } from '../../services/auth.service';
-import { LoginButtonsComponent } from '../../components/login-buttons/login-buttons.component';
 import { RouterModule } from '@angular/router';
+import { TopBarComponent } from '../../components/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-home',
@@ -20,19 +17,16 @@ import { RouterModule } from '@angular/router';
     CalendarComponent,
     AddAvailabilityComponent,
     AddAbsenceComponent,
-    SwitchSourceComponent,
-    LogOutComponent,
-    SwitchDoctorComponent,
-    LoginButtonsComponent,
     RouterModule,
+    TopBarComponent,
   ],
 })
 export class HomeComponent {
   constructor(private authService: AuthService) {}
 
+  user?: User;
+
   ngOnInit() {
     this.user = this.authService.currentUserValue?.user;
   }
-
-  user?: User;
 }
